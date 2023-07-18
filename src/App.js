655d4f0c8bef62f581f34071a6f7f22974a07aa6
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-07-18 17:50:42 +0530

    remove comments

--- src/App.js
+++ src/App.js
@@ -24,11 +24,7 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
-  /* return React.createElement(
-    'div',{},
-    React.createElement('h2',{},"Let's get started!"),
-    React.createElement(Expenses,{expenses:expenses})
-  ) */
+
   return (
     <div>
       <NewExpense />
