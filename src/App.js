e04824d5ab635543beedc64da13f44c7073f32ff
madhurshani-sivan <madhurshanisivan@gmail.com>
2023-07-18 10:58:15 +0530

    change to arrow functions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Expenses from "./components/Expenses/Expenses";
 import React from "react";
 
-function App() {
+const App = () => {
   const expenses = [
     {
       id: "e1",
@@ -34,6 +34,6 @@ function App() {
       <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
